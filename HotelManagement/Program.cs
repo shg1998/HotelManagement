4d59d0584eae55ab5bb23e81e407562027e80818
shg1998 <moh.nezh1377@gmail.com>
2022-07-15 10:40:34 +0430

    add Serilog for logging in .txt Files:)

--- HotelManagement/Program.cs
+++ HotelManagement/Program.cs
@@ -1,5 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace HotelManagement
 {
@@ -7,11 +10,31 @@ namespace HotelManagement
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                    path: @"G:\Projects\Git\HotelManagement\logs\log-.txt",
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.ff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day,
+                    restrictedToMinimumLevel: LogEventLevel.Information
+                ).CreateLogger();
+            try
+            {
+                Log.Information("Application Is Starting");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+               Log.Fatal(e,"Application Failed To Start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
